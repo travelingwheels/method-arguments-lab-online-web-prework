c4d2d3a2818bf1159ea6b98dfdad43e7641fb97f
Gary Wheeler <gkwheeler40@gmail.com>
2019-03-22 18:13:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-# lib/introduction_with_language.RUBY_VERSION
 def introduction_with_language(name, language)
   puts "Hi my name is #{name} and I am learning to program in #{language}."
 end
